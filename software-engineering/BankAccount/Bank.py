ab3578d010b5214ad93ca8562efeb9fcc0fc50cb
Sebastian Lopez-Gallardo <jusepez90@gmail.com>
2025-02-13 19:42:33 -0500

    added saving account

--- software-engineering/BankAccount/Bank.py
+++ software-engineering/BankAccount/Bank.py
@@ -1,38 +1,80 @@
-class bank:
+'''
+Author:Sebastian Lopez-Gallardo
+Date:1/29/2025
+Description:Program that simulates bank in order to practice OPP in python
+'''
+
+class Bank:
     title = ""
     
-
+    #Constructor
     def __init__(self, title, customer_name, current_balance, minimum_balance):     
         self.title = title
         self.customer_name = customer_name
         self.current_balance = current_balance
         self.minimum_balance = minimum_balance
 
+    #Check if the entered value is valid if so adds it otherwise it return error message adn continues working
     def deposit(self, amount):
-        if (amount.isdigit() == False or amount <= 0):
+        try:
+            amount = float(amount)
+            self.current_balance += amount
+            return "Succesfully deposited $" + str(amount)
+        except ValueError:
             return "Enter valid number!"
-        self.current_balance += amount
-        return "Succesfully deposited $" + str(amount)
     
+    #Check if the entered value is valid if so adds it otherwise it return error message adn continues working
     def withdraw(self, amount):
-        if (amount.isdigit() == False or amount <= 0):
-            return "Enter valid number!"
+        try:
+            amount = float(amount)
+            if (amount <= 0):
+                return "Enter valid number!"
+            
+            if ((self.current_balance-amount) < self.minimum_balance):
+                return "Remaining balance cannot be less than " + str(self.minimum_balance)
         
-        if ((self.current_balance-amount) < self.minimum_balance):
-            return "Remaining balance cannot be less than " + str(self.minimum_balance)
+            self.current_balance -= amount
+            return "Succesfully withdrawn $" + str(amount) + "\nRemaining balance: $" + str(self.current_balance)
+        except ValueError:
+            return "Enter valid number!"
 
-        self.current_balance -= amount
-        return "Succesfully withdrawn $" + str(amount) + "\nRemaining balance: $" + str(self.current_balance)
 
+    #Displays bank object as string
     def __str__(self):
         total = "Bank: %s\nCustomer's Name: %s\nCurrent balance:%s\n" % (self.title, self.customer_name, self.current_balance)
         return total
     
 
-b1 = bank("Truist", "Jose", 10, 1)
+class SavingAccount(Bank):
 
-b2 = bank("Chase", "John", 100, 10)
+    def __init__(self, title, customer_name, current_balance, minimum_balance, account_number, routing_number, interest_rate):
+        super().__init__(title, customer_name, current_balance, minimum_balance)
+        self.account_number = account_number
+        self.routing_number = routing_number
+        self.interest_rate = interest_rate
 
-print(b1.deposit(100))
+
+
+
+    
+
+b1 = Bank("Truist", "Jose", 10, 1)
+
+b2 = Bank("Chase", "John", 100, 10)
+
+#testing
+print(b1.deposit("j"))
+print(b1.deposit(50000))
+print(b1.deposit("j"))
+print(b1.deposit(50))
+print(b1.deposit("j"))
 print(b1.withdraw(50))
+print(b1.deposit("j"))
+print(b1.withdraw("-9050000"))
+print(b1.withdraw("j"))
+print(b1.withdraw(50))
+print(b1.withdraw(-50000))
+
+print(b1)
+
 
